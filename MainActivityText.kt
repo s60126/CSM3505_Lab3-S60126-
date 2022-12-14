4d1591151f6f9bd27fd95764472b28b962074c64
s60126 <103713646+s60126@users.noreply.github.com>
2022-12-14 15:49:30 +0800

    Update MainActivityText.kt

--- MainActivityText.kt
+++ MainActivityText.kt
@@ -46,9 +46,3 @@ fun BirthdayCardPreviewPreview() {
         BirthdayGreetingWithText( "Happy Birthday Sam!", "- from Emma")
     }
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
